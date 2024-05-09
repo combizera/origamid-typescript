0d369f33c8c4ea2c74ea0c43eeb2b8665434a2e5
combizera <ycombi349@gmail.com>
2024-05-09 15:56:22 -0300

    FEAT: union types

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 "use strict";
-const frase1 = new String('Front End');
-const frase2 = String('Front End');
-const frase3 = "Front End";
-console.log(typeof frase1);
-console.log(frase1);
-console.log(typeof frase2);
-console.log(typeof frase3);
+let total = 200;
+total = "4000";
+function isNumber(value) {
+    if (typeof value === 'number') {
+        console.log('É um número');
+        return true;
+    }
+    else {
+        console.log('Não é um número');
+    }
+}
+console.log(isNumber(200));
--- script.ts
+++ script.ts
@@ -1,9 +1,15 @@
-const frase1 = new String('Front End');
-const frase2 = String('Front End');
-const frase3 = "Front End";
+let total: string | number = 200;
+total = "4000";
 
-console.log(typeof frase1);
-console.log(frase1);
-console.log(typeof frase2);
-console.log(typeof frase3);
+function isNumber(value: string | number){
+  if(typeof value === 'number'){
+    console.log('É um número');
+    return true;
+  }
+  else{
+    console.log('Não é um número');
+  }
 
+}
+
+console.log(isNumber(200));
