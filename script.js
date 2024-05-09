4f3e6294b0f82fd2712b429068bcc8c648f63db2
combizera <ycombi349@gmail.com>
2024-05-09 15:27:51 -0300

    FEAT: string, number and boolean

--- script.js
+++ script.js
@@ -1,28 +1,8 @@
 "use strict";
-// Exercício 1
-function normalizarTexto(texto) {
-    return texto.trim().toLowerCase();
-}
-console.log(normalizarTexto(' deSiNG de MarcasSSS'));
-// Exercício 2
-const $input = document.querySelector('input');
-const total = localStorage.getItem('total');
-if ($input && total) {
-    $input.value = total;
-    calcularGanho(Number($input.value));
-}
-function calcularGanho(value) {
-    const $p = document.querySelector('p');
-    if ($p) {
-        $p.innerText = `Ganho total: ${value + 100 - value * 0.2}`;
-    }
-}
-function totalMudou() {
-    if ($input) {
-        localStorage.setItem("total", $input.value);
-        calcularGanho(Number($input.value));
-    }
-}
-if ($input) {
-    $input.addEventListener('keyup', totalMudou);
-}
+const frase1 = new String('Front End');
+const frase2 = String('Front End');
+const frase3 = "Front End";
+console.log(typeof frase1);
+console.log(frase1);
+console.log(typeof frase2);
+console.log(typeof frase3);
