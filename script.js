855cfb79a8ff63741bb66df8cc2cc23df86d1fc3
combizera <ycombi349@gmail.com>
2024-05-11 14:01:22 -0300

    FEAT: exercicios array

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 "use strict";
-const numeros = [10, 20, 40, 5, 51, 4];
-const valores = ["1", "20", "diambaa", 40, 5, 51, 4];
-const livros = [
-    ["O senhor dos aneis", 80],
-    ["A Guerra dos Tronos", 120],
-];
-function maiorQue10(data) {
-    return data.filter((n) => n > 10);
+// Receber os dados da API se o curso for iniciante pinte o titulo de azul, se for avançado pinte de vermelho
+async function fetchCursos() {
+    const response = await fetch('https://api.origamid.dev/json/cursos.json');
+    const data = await response.json();
+    mostrarCursos(data);
+    // mostrar API no console
+    console.log(data);
 }
-function filtrarNumeros(data) {
-    return data.filter(item => typeof item === "number");
+fetchCursos();
+function mostrarCursos(cursos) {
+    const $ul = document.querySelector('#cursos ul');
+    if ($ul) {
+        cursos.forEach(curso => {
+            const $li = document.createElement('li');
+            let color;
+            if (curso.nivel === 'iniciante') {
+                color = 'blue';
+            }
+            else if (curso.nivel === 'avancado') {
+                color = 'red';
+            }
+            $li.innerHTML += `
+        <h3 style="color:${color};">${curso.nome}</h3>
+        <div class="cursos__tags">
+          Tags:
+          <span>${curso.tags}</span>
+        </div>
+        <div class="cursos__infos">
+          <p>Tipo: ${curso.gratuito ? 'Gratuito' : 'Pago'}</p>
+          <p>Horas: ${curso.horas}h</p>
+          <p>Aulas: ${curso.aulas}</p>
+        </div>
+      `;
+            $ul.appendChild($li);
+        });
+    }
+    else {
+        console.error("Não há cursos");
+    }
 }
-console.log(maiorQue10(numeros));
-console.log(filtrarNumeros(valores));
