6b7268a94c89f7f29a58aece83d105ada6537e77
combizera <ycombi349@gmail.com>
2024-05-13 10:45:54 -0300

    FEAT: interfaces DOM 3 - Exercicios

--- script.ts
+++ script.ts
@@ -1,12 +1,22 @@
+// 1 - Selecione os elementos com a classe link.
+// 2 - Crie uma função que deve ser executada para cada elemento.
+// 3 - Modificar através da função o estilo da color e border.
+
 const $links = document.querySelectorAll('.link');
 
+// console.log($links);
+
 $links.forEach(($link) => {
-  if($link instanceof HTMLAnchorElement){
-    console.log($link);
+  let color;
+  let border;
+  if ($link instanceof HTMLElement){
+    color = 'green';
+    border = '2px solid green';
+    if($link instanceof HTMLAnchorElement){
+      color = 'tomato';
+      border = '2px solid tomato';
+    }
+    $link.style.color = color;
+    $link.style.border = border;
   }
 })
-// Aqui é um array
-const array = [1,2,3];
-
-// Transformamos em array
-const arrayLinks = Array.from($links);
