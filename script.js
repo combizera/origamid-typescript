83e9e8e9d660f12858072571f744edaf074926e8
combizera <ycombi349@gmail.com>
2024-05-13 10:52:40 -0300

    FEAT: interfaces DOM 3 - Exercicios versão André Rafael

--- script.js
+++ script.js
@@ -2,19 +2,13 @@
 // 1 - Selecione os elementos com a classe link.
 // 2 - Crie uma função que deve ser executada para cada elemento.
 // 3 - Modificar através da função o estilo da color e border.
-const $links = document.querySelectorAll('.link');
-// console.log($links);
-$links.forEach(($link) => {
-    let color;
-    let border;
-    if ($link instanceof HTMLElement) {
-        color = 'green';
-        border = '2px solid green';
-        if ($link instanceof HTMLAnchorElement) {
-            color = 'tomato';
-            border = '2px solid tomato';
-        }
-        $link.style.color = color;
-        $link.style.border = border;
+const links = document.querySelectorAll('.link');
+links.forEach((item) => {
+    if (item instanceof HTMLElement) {
+        ativarElemento(item);
     }
 });
+function ativarElemento(elemento) {
+    elemento.style.color = 'red';
+    elemento.style.border = '2px solid red';
+}
