fc4feeb4152b54faaab0766d64b115b480cee2db
combizera <ycombi349@gmail.com>
2024-05-14 17:13:22 -0300

    FEAT: eventos and callback 3 - exercicios

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 "use strict";
-const $button = document.querySelector('button');
-function handleClick(event) {
-    const $element = event.currentTarget;
-    if ($element instanceof HTMLElement) {
-        console.log($element);
+// Estado dos elementos
+// menu inativo:
+// class="" em nav
+// aria-expanded="false" em button
+// aria-label="Abrir Menu" em button
+// menu ativo:
+// class="active" em nav
+// aria-expanded="true" em button
+// aria-label="Fechar Menu" em button
+const $button = document.getElementById('btn-mobile');
+if ($button instanceof HTMLElement) {
+    let $btnAriaLabel = $button.ariaLabel;
+    let $btnAriaExpanded = $button.ariaExpanded;
+    function handleClick() {
+        $button?.classList.toggle('active');
+        if ($button?.classList.contains('active')) {
+            $btnAriaLabel = 'Fechar Menu';
+            $btnAriaExpanded = 'true';
+        }
+        else {
+            $btnAriaLabel = 'Abrir Menu';
+            $btnAriaExpanded = 'false';
+        }
+        $button?.setAttribute('aria-label', $btnAriaLabel);
+        $button?.setAttribute('aria-expanded', $btnAriaExpanded);
     }
+    window.addEventListener('click', handleClick);
 }
-// handleClick();
-$button?.addEventListener('click', handleClick);
--- script.ts
+++ script.ts
@@ -1,13 +1,35 @@
-const $button = document.querySelector('button');
+// Estado dos elementos
 
-function handleClick(event: MouseEvent){
-  const $element = event.currentTarget;
+// menu inativo:
+// class="" em nav
+// aria-expanded="false" em button
+// aria-label="Abrir Menu" em button
 
-  if($element instanceof HTMLElement){
-    console.log($element);
+// menu ativo:
+// class="active" em nav
+// aria-expanded="true" em button
+// aria-label="Fechar Menu" em button
+
+const $button = document.getElementById('btn-mobile');
+
+if($button instanceof HTMLElement){
+  let $btnAriaLabel = $button.ariaLabel;
+  let $btnAriaExpanded = $button.ariaExpanded;
+
+  function handleClick(){
+  
+    $button?.classList.toggle('active');
+
+    if($button?.classList.contains('active')){
+      $btnAriaLabel = 'Fechar Menu';
+      $btnAriaExpanded = 'true';
+    } else {
+      $btnAriaLabel = 'Abrir Menu';
+      $btnAriaExpanded = 'false';
+    }
+    $button?.setAttribute('aria-label', $btnAriaLabel);
+    $button?.setAttribute('aria-expanded', $btnAriaExpanded);
   }
+
+  window.addEventListener('click', handleClick);
 }
-
-// handleClick();
-
-$button?.addEventListener('click', handleClick);
