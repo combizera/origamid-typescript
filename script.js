7972d5f6b96df52d2596829e71c36905ed594821
combizera <ycombi349@gmail.com>
2024-05-18 19:48:24 -0300

    FEAT: functions exercicio - Andre Rafael

--- script.js
+++ script.js
@@ -3,19 +3,12 @@
 // A função pode receber string ou number;
 // A função deve retornar o mesmo tipo que ela receber
 function arredondar(valor) {
-    if (typeof valor === 'string') {
-        console.log("Valor informado é uma string, não é possível arredondar");
-        return;
-    }
     if (typeof valor === 'number') {
-        let numero = Number.isInteger(valor);
-        if (numero) {
-            console.log(`O número '${valor}' já é inteiro`);
-            return;
-        }
-        let valorAumentado = Math.ceil(valor);
-        console.log("Arredondando para cima...");
-        console.log(`Resultado: ${valorAumentado}`);
+        return Math.ceil(valor);
+    }
+    else {
+        return Math.ceil(Number(valor)).toString();
     }
 }
-(arredondar(1.0));
+console.log(arredondar(200.32));
+console.log(arredondar('200.32'));
--- script.ts
+++ script.ts
@@ -2,24 +2,15 @@
 // A função pode receber string ou number;
 // A função deve retornar o mesmo tipo que ela receber
 
-function arredondar(valor: string | number): string | number | any{
-  if(typeof valor === 'string') {
-    console.log("Valor informado é uma string, não é possível arredondar");
-    return;
-  }
-
-  if(typeof valor === 'number'){
-    let numero = Number.isInteger(valor);
-
-    if (numero){
-      console.log(`O número '${valor}' já é inteiro`);
-      return;
-    }
-
-    let valorAumentado = Math.ceil(valor);
-    console.log("Arredondando para cima...");
-    console.log(`Resultado: ${valorAumentado}`);
+function arredondar(valor: string): string;
+function arredondar(valor: number): number;
+function arredondar(valor: string | number): string | number {
+  if (typeof valor === 'number') {
+    return Math.ceil(valor);
+  } else {
+    return Math.ceil(Number(valor)).toString();
   }
 }
 
-(arredondar(1.0));
+console.log(arredondar(200.32));
+console.log(arredondar('200.32'));
